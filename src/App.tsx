a8a9b77828afc7296ae9f58f5c32fe7695222c1d
Yuna Jang <syjak7@gmail.com>
2022-04-28 09:46:13 +0900

    0428 First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () =>  {
+  return (
+    <div className="App">
+      <h2>Hello~ my React</h2>
+    </div>
+  );
+}
+
+export default App;
